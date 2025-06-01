16ae199d450a3393ab27dc21e998d87c6cea0e35
skanpl <savankan089@gmail.com>
2025-06-01 03:02:18 +0200

    finished proof of lemma6

--- proc.v
+++ proc.v
@@ -145,67 +145,190 @@ Lemma lemma6: forall P Q P' a,
   lt P a P'-> conga P Q -> exists Q', lt Q a Q' /\ conga P' Q'.
 Proof.
 intros. 
-induction H.
-(*===base cases===*)
-   exfalso. inversion H0.
-   exfalso.  inversion H0.
-(*===indcase parL===*)
-   
-  (*----caseAn on conga---------*) inversion H0. 
-    (*subcase com *)
-      exists (Par Q0 P'). split.
-      apply (Lt_parR Q0 P P' a).
-      apply H.
-      apply (Cga_parCom P' Q0).
-    (*subcase assoc *) 
-      (* caseAn on P->a P*)  inversion H.  
-        (*subsubcase send/rcv*)
-            subinall_lt.   
-            exfalso. inversion H2.
-            subinall_lt. 
-            exfalso. inversion H2.    
+inversion H.  (*caseAn on lt derivation*)
+(*======all the cases of Lt_send======*)  
+inversion H0. 
+subinall. 
+exfalso. inversion H0.
+subinall. 
+exfalso. inversion H0.
+subinall. 
+exfalso. inversion H0.
+(* ======all the cases of Lt_rcv======*)
+inversion H0.
+subinall.  
+exfalso. inversion H0.
+subinall. 
+exfalso. inversion H0.
+subinall. 
+exfalso. inversion H0.
+(*======all the cases parL======*)
+inversion H0.
+(*subcase com*)
+subinall.  
+inversion H2. (*destroy the AST*)
+rewrite H2 in H0.
+rewrite H2 in H.
+rewrite H9 in H.
+rewrite H8 in H1.
+exists (Par Q1 P'0). split.
+apply (Lt_parR Q1 P1  P'0 a).
+apply H1.
+apply (Cga_parCom P'0 Q1).
+(*subcase assoc*)  
+
+subinall. 
+inversion H2. (*destruct the AST*)
+rewrite H8 in H1.
+  (*caseAn on P1|Q1 ->a P'0*)
+  inversion H1.
+  exists (Par P'1 (Par Q1 R)). split.
+  apply (Lt_parL  (Par Q1 R) P1 P'1 a ).
+  apply H13.
+  apply (Cga_parAssoc P'1 Q1 R).
+  
+  exists (Par P1 (Par Q' R)). split.
+  apply (Lt_parR P1 (Par Q1 R) (Par Q' R) a).
+  apply (Lt_parL R Q1 Q' a).
+  apply H13.
+  apply (Cga_parAssoc P1 Q' R).
+  
+  exists ( Par P'1 (Par Q' R) ). split.
+  apply (Lt_commL P1 (Par Q1 R) P'1 (Par Q' R) q r).
+  apply H11. 
+  apply (Lt_parL R Q1 Q' (Lrcv q r)).
+  apply H14.
+  apply (Cga_parAssoc P'1 Q' R). 
+
+  exists (Par P'1 (Par Q' R)). split.
+  apply (Lt_commR P1 (Par Q1 R) P'1 (Par Q' R) q r).
+  apply H11.
+  apply (Lt_parL  R Q1 Q' (Lsend q r)).
+  apply H14.
+  apply (Cga_parAssoc P'1 Q' R).
+(*subcase neut*)
+  subinall.
+  inversion H2. (*destroy the AST*)
+  rewrite H8 in *.
+  exists (P'0). split.
+  apply H1.
+  apply (Cga_parNeut P'0).
+  
+(*======all the cases parR======*)
+inversion H0.
+(*subcase com*)
+subinall. 
+inversion H2. (*destroy the AST*)
+symmetry in H9;rewrite H9.
+exists (Par Q' P1). split.
+apply (Lt_parL P1 Q0 Q' a).
+apply H1.
+apply (Cga_parCom P1 Q').
+(*subcase assoc*)
+subinall.  
+
+(* caseAn on conga*)
+inversion H0.
+
+exists (Par Q' (Par Q1 R)). split.
+rewrite H10 in *.
+apply (Lt_parL (Par Q1 R) P1 Q' a).
+apply H1.
+apply (Cga_parCom (Par Q1 R) Q').
  
-           
-        (*subsubcase parL*)
-            (** --i adapt the notations to mine--*)
-            (**) subinall. subinall. 
-            (**) set (Ptilde := P0) in *. set (Qtilde := Q1) in *. 
+exists (Par P1 (Par Q1 Q')). split.
+rewrite H12 in *.
+apply (Lt_parR P1 (Par Q1 R) (Par Q1 Q') a).
+apply (Lt_parR  Q1 R Q' a).
+apply H1.
+apply (Cga_parAssoc P1 Q1 Q').
 
-            (**) inversion H2. (*deconstruct the AST*) 
-            (**) symmetry in H9; rewrite H9 in *; symmetry in H10; rewrite H10 in *.
-            (**) set (Ptilde' := P'0) in *. 
-            (** ---------------*)
-   
-             exists (Par Ptilde' (Par Qtilde Q0)). split. 
-             apply (  Lt_parL (Par Qtilde Q0) Ptilde Ptilde' a         ).
-             apply H4.   
-             clear .  
-             apply (Cga_parAssoc Ptilde' Qtilde Q0). 
-        (*subsubcase parR*)
-             subinall.   subinall. 
-             set (Ptilde := P0) in*. set (Qtilde := Q1) in * .  
-               
-             inversion H2. (*destruct the AST*)
-             symmetry in H9, H10 . rewrite  H9,H10 in *. 
-             exists (Par Ptilde (Par Q' Q0)). split.
-             apply (Lt_parR Ptilde (Par Qtilde Q0) (Par Q' Q0) a).
-             apply (Lt_parL Q0 Qtilde Q' a).
-             apply H4.
-             apply (Cga_parAssoc Ptilde Q' Q0).
-        (*subsubcase commL*)       
-             set (Ptilde := P1) in *. set (Ptilde' := P'0) in *.
-             set (Qtilde := Q2) in *. set (Qtilde':= Q') in *. 
-             exists (Par Ptilde' (Par Qtilde' Q0)). split.
-             symmetry in H6. rewrite H6 in *. 
-             inversion H2. (*destruct the AST*) 
-             rewrite H10,H11 in *.
-             apply (Lt_commL Ptilde (Par Qtilde Q0) Ptilde' (Par Qtilde' Q0) q r).
-             apply H4. apply (Lt_parL Q0 Qtilde Qtilde' (Lrcv q r)). apply H5.
-             apply (Cga_parAssoc Ptilde' Qtilde' Q0).
-        (*subsubcase commR*)
-                               
+subinall. 
+rewrite H7 in *.
+exfalso. inversion H1.
 
+(*subcase neut*)
+subinall. 
+inversion H2. (*destroy AST*)
+rewrite H9 in *.
+(*caseAn on conga*)
+inversion H0.
+exfalso. inversion H1.
+exfalso. inversion H1.
+exfalso. inversion H1.
 
+(* ======all the cases of Lt_commL======*)
+inversion H0. (*caseAn on conga*)
 
+subinall. 
+inversion H3. (*destruct AST*)
+rewrite H9, H10 in *.
+symmetry in H4;rewrite H4 in H.
+exists (Par Q' P'0). split.
+apply (Lt_commR Q1 P1 Q' P'0 q r).
+apply H2. apply H1.
+apply (Cga_parCom P'0 Q').
 
+subinall.
+inversion H3. (*destruct AST*)
+rewrite H9 in *.
+symmetry in H4;rewrite H4 in H.   
+inversion H1.  
+(*caseAn on   P1|Q1 ->(q!r)   P'0*)  
+(**)
+rewrite H10 in H2. 
+exists (Par P'1 (Par Q1 Q')). split.
+apply (Lt_commL P1 (Par Q1 R) P'1 (Par Q1 Q') q r).
+apply H14.
+apply (Lt_parR Q1 R Q' (Lrcv q r)).
+apply H2.
+apply (Cga_parAssoc P'1 Q1 Q').
+(**)
+rewrite H10  in H2.
+exists (Par P1 (Par Q'0 Q')). split.
+apply (Lt_parR P1 (Par Q1 R) (Par Q'0 Q') Ltau).
+apply (Lt_commL Q1 R Q'0 Q' q r).
+apply H14.
+apply H2.
+apply (Cga_parAssoc P1 Q'0 Q').
 
+subinall. 
+inversion H3. (*destroy AST*)
+rewrite H10 in *.
+exfalso. inversion H2.
+(* ======all the cases of Lt_commR======*)
+inversion H0.
+
+subinall.
+inversion H3. (*destroy AST*)
+rewrite H9,H10 in *.
+exists (Par Q' P'0). split.
+apply (Lt_commL Q1 P1 Q' P'0 q r).
+apply H2. apply H1.
+apply (Cga_parCom P'0 Q').
+
+subinall.
+inversion H3. (*destroy AST*)
+subinall.
+rewrite H10 in H2. rewrite H9 in H1.
+(*caseAn on   P1|Q1 ->(q?r)   P'0*)
+inversion H1.
+exists (Par P'1 (Par Q1 Q')). split.
+apply (Lt_commR P1 (Par Q1 R) P'1 (Par Q1 Q') q r).
+apply H14. 
+apply (Lt_parR Q1 R Q' (Lsend q r)). apply H2.
+apply (Cga_parAssoc P'1 Q1 Q').
+(**) 
+exists (Par P1 (Par Q'0 Q')). split.
+apply (Lt_parR P1 (Par Q1 R) (Par Q'0 Q') Ltau).
+apply (Lt_commR Q1 R Q'0 Q' q r).
+apply H14. apply H2.
+apply (Cga_parAssoc P1 Q'0 Q').
+
+subinall. 
+inversion H3. (*destroy AST*)
+rewrite H10 in *.
+exfalso. inversion H2.
+Qed.
+
+ 
