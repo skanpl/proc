84776443fc8137b9fec2053e0eb3b7d6382b567f
skanpl <savankan089@gmail.com>
2025-06-25 19:48:08 +0200

    trying to prove cong is a bisim, seems like we need the nu rules seperated from conga

--- theories/Semantics.v
+++ theories/Semantics.v
@@ -58,13 +58,10 @@ Inductive conga: proc -> proc -> Prop :=
 | Cga_parCom: forall P Q,     conga (Par P Q)  (Par Q P)
 | Cga_parAssoc: forall P Q R, conga (Par (Par P Q) R)    (Par P (Par Q R))
 | Cga_parNeut: forall P,      conga (Par P Zero) P
-.
 
-
-Inductive congb: proc -> proc -> Prop :=
-| Cgb_nuZero: congb (Nu Zero) Zero
-| Cgb_nuPar: forall P Q,  congb (Par (Nu P) Q)   (Nu (Par P (Q [shift_sb]) ))
-| Cgb_nuSwap: forall P, congb (Nu (Nu P))  (Nu (Nu (swap P)))
+| Cgb_nuZero: conga (Nu Zero) Zero
+| Cgb_nuPar: forall P Q,  conga (Par (Nu P) Q)   (Nu (Par P (Q [shift_sb]) ))
+| Cgb_nuSwap: forall P, conga (Nu (Nu P))  (Nu (Nu (swap P)))
 .
 
 
@@ -75,7 +72,6 @@ Inductive congb: proc -> proc -> Prop :=
 (*  cong = conga + equivrel + ctxrules *)
 Inductive cong: proc -> proc -> Prop :=
 | Cg_cga: forall P Q,     conga P Q -> cong P Q
-| Cg_cgb: forall P Q,     congb P Q -> cong P Q
 | Cg_refl: forall P,       cong P P
 | Cg_sym: forall P Q,     cong Q P -> cong P Q
 | Cg_trans: forall P Q R, cong P Q -> cong Q R -> cong P R
@@ -96,20 +92,25 @@ Inductive lab :=
 .
 
 
-Definition not_bdsend a := 
-  exists x y, a = Lsend x y \/
-  exists x y, a = Lrcv x y  \/
-  a = Ltau
+
+Definition not_bdsend a :=
+  a = Ltau \/ 
+  (exists x y, a = Lsend x y) \/
+  (exists x y, a = Lrcv x y)  
 .
 
 Definition notinlab a u := 
-  (exists x y, a = Lsend x y -> ~(u = x) /\ ~(u = y) ) \/
-  (exists x y, a = Lrcv x y -> ~(u = x) /\ ~(u = y) ) \/
-  (exists x y, a = LbdSend x y -> ~(u = x) /\ ~(u = y) ) .
+  a = Ltau \/
+  forall x y, 
+  (a = Lsend x y -> ~(u = x) /\ ~(u = y) )   /\
+  (a = Lrcv x y -> ~(u = x) /\ ~(u = y) )    /\
+  (a = LbdSend x y -> ~(u = x) /\ ~(u = y) ) .
 
 
 
-
+(*
+maybe the Lt_res rule should be modified later?
+*)
 Inductive lt: proc -> lab -> proc -> Prop :=
 | Lt_send: forall x y P, lt (Send x y P) (Lsend x y) P 
 | Lt_rcv: forall x P y, lt (Rcv x P) (Lrcv x y) (P [y ..])
@@ -139,15 +140,15 @@ Inductive lt: proc -> lab -> proc -> Prop :=
   lt P (Lsend (ch x) (ch 0)) P' ->  x>0  -> 
      lt (Nu P) (LbdSend (ch x) (ch 0)) P'
 | Lt_res: forall P P' a,  
-   lt P a P' -> notinlab a u -> 
+   lt P a P' -> notinlab a (ch 0) -> 
      lt (Nu P) a (Nu P')
 
 | Lt_closeL: forall P P' Q Q' x, 
-  lt P (Lbdsend x (ch0)) P' -> lt (Q[shift_sb]) (Rcv x (ch 0)) Q' -> 
+  lt P (LbdSend x (ch 0)) P' -> lt (Q[shift_sb]) (Lrcv x (ch 0)) Q' -> 
     lt (Par P Q) Ltau (Nu (Par P' Q'))
     
 | Lt_closeR: forall P P' Q Q' x, 
-  lt (P[shift_sb]) (Lrcv x (ch 0)) P' -> lt Q (Lbdsend x (ch0)) Q' ->
+  lt (P[shift_sb]) (Lrcv x (ch 0)) P' -> lt Q (LbdSend x (ch 0)) Q' ->
     lt (Par P Q) Ltau (Nu (Par P' Q'))  
 .
 (*========================*)
@@ -162,8 +163,7 @@ Inductive red: proc -> proc -> Prop :=
 .
 
  
-Hint Constructors congb chan proc conga cong lab lt red: picalc. 
-
+Hint Constructors chan proc conga cong lab lt red: picalc. 
 
 Fixpoint iter_nu n P := match n with
  | 0   => P
@@ -172,19 +172,85 @@ Fixpoint iter_nu n P := match n with
 
 
 
+Lemma not_bdsend_rcv: forall x y, not_bdsend (Lrcv x y).
+Proof. 
+intros.  
+cbv. right. right. eauto.
+Qed.
+
+Lemma not_bdsend_send: forall x y, not_bdsend (Lsend x y).
+Proof.
+intros.
+cbv. right. left. eauto.
+Qed.
+
+Lemma not_bdsend_tau: not_bdsend (Ltau).
+Proof.
+cbv. auto. 
+Qed.
+
+ 
+Hint Resolve not_bdsend_rcv not_bdsend_send not_bdsend_tau: picalc. 
 
 
 
-(* lt before:
 
-Inductive lt: proc -> lab -> proc -> Prop :=
-| Lt_send: forall x y P, lt (Send x y P) (Lsend x y) P 
-| Lt_rcv: forall x P y, lt (Rcv x P) (Lrcv x y) (P [y ..]) 
-| Lt_parL: forall Q P P' a, lt P a P' -> lt (Par P Q) a (Par P' Q) 
-| Lt_parR: forall P Q Q' a, lt Q a Q' -> lt (Par P Q) a (Par P Q')
-| Lt_commL: forall P Q P' Q' x y, 
-    lt P (Lsend x y) P' -> lt Q (Lrcv x y) Q' -> lt (Par P Q) Ltau (Par P' Q')
-| Lt_commR: forall P Q P' Q' x y, 
-    lt P (Lrcv x y) P' -> lt Q (Lsend x y) Q' -> lt (Par P Q) Ltau (Par P' Q')
-.
+Lemma Lconga_resp_lt: forall P Q P' a, 
+  lt P a P'-> conga P Q -> exists Q', lt Q a Q' /\ conga P' Q'.
+Proof.
+intros. 
+inversion H. (*caseAn on P -->a  P' *)
+(*====== base cases ======*)  
+subst. inversion H0.
+subst. inversion H0.
+(*====== free parL ======*)
+subst. 
+inversion H0. (*caseAn on conga*)
+
+(* case commut*)
+subst.
+eexists. split.
+eauto with picalc.
+eauto with picalc.
+
+(*case assoc*)
+subst.     
+inversion H1; eauto with picalc. (* caseAn on P|Q1 -->a P'0 *) 
+eexists. split. eauto with picalc.
+subst.  
+firstorder; inversion H2.
+ 
+eexists. split. eauto with picalc. 
+subst. 
+firstorder; inversion H2.   
+ 
+subst.    
+eexists. split. 
+eapply Lt_closeL. eauto with picalc.
+cbn.  
+eapply Lt_parL.
+eauto with picalc. 
+eauto with picalc.
+eauto with picalc.
+
+(* goal: (seems impossible without transitivvity ...)
+the reasonable way would be to do "sc_extr+assoc"
+     (Nu (P'|Q')) | Q0
+       conga
+     Nu (  P'| (Q'|Q0[shift])   )
 *)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
