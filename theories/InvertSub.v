e98020769b8289fbc4b76e4f618b79f388c504d1
skanpl <savankan089@gmail.com>
2025-07-18 19:20:12 +0200

    some further reorg

--- theories/InvertSub.v
+++ theories/InvertSub.v
@@ -240,66 +240,6 @@ Qed.
 
 
 
-(*
-Lemma invert_sb_lt: forall P P0 a Q sigma, lt P a Q -> 
-  P = P0[sigma] -> super_bij sigma ->  
-  exists P' a0, Q = P'[sigma] /\  a = a0[sigma] /\  lt P0 a0 P'. 
-Proof.
-intros. 
-generalize dependent sigma.
-generalize dependent P0.
-induction H; intros.   
-- set (lem:= invert_sb_send).
-  specialize (lem _ _ _ _ _ H0).
-  destruct lem. do 2 destruct H. subst. cbn in *.
-  inversion H0. 
-  eexists. exists (Lsend x0 x1). 
-  split; eauto with picalc. 
-
--set (lem:= invert_sb_rcv).
-  specialize (lem _ _ _ _ H0).
-  destruct lem. destruct H as [p H]. subst. cbn in *.
-  inversion H0.
-  unfold super_bij in H1. destruct H1.
-  assert (exists y0, y = y0[sigma]). eapply H.
-  destruct H4 as [y0 H4].
-  exists (p[y0 ..]). exists (Lrcv x0 y0). 
-  split; cbn; try erewrite up_beta_simpl_pr; rewrite H4; eauto with picalc.
-
-- set (lem:= invert_sb_par).
-  specialize (lem _ _ _ _ H1).
-  destruct lem as [P1 [Q1 lem]].  subst.
-  cbn in H1. inversion H1. rewrite H4 in IHlt.
-  specialize (IHlt P1 sigma eq_refl H2).
-  destruct IHlt as [P'0 [a0 IHlt]].  destruct IHlt.
-  destruct H6.  rewrite H3. 
-  exists (Par P'0 Q1). eexists. split; eauto with picalc.
-  split. apply H6. eapply Lt_parL. auto.
-  eapply (not_bdsend_sub_rev _ sigma).
-  symmetry in H6. rewrite H6. auto.
-
-- set (lem:= invert_sb_par _ _ _ _ H1).  
-  destruct lem as [p [q lem]]. subst. cbn in H1.
-  inversion H1. subst.
-  specialize (IHlt _ _ eq_refl H2).
-  destruct IHlt as [P' [a0 IHlt]].
-  destruct IHlt as [IHlt1 [IHlt2 IHlt3]].
-  rewrite IHlt1.
-  exists (Par p P'). repeat eexists. 
-  eauto with picalc. 
-  eapply Lt_parR. auto.
-  eapply (not_bdsend_sub_rev _ sigma).
-  symmetry in IHlt2. rewrite IHlt2. auto.
-
- 
-- set (lem:= invert_sb_par _ _ _ _ H1).  
-  destruct lem as [p [q lem]]. subst. cbn in H1.
-  inversion H1. subst.
-  specialize (IHlt _ _ eq_refl H2).
-  destruct IHlt as [p' [a0 IHlt]].
-  destruct IHlt as [IHlt1 [IHlt2 IHlt3]].
-  rewrite IHlt1. 
- *)
 
 
 
@@ -324,9 +264,16 @@ Lemma invert_sb_lt: forall P P0 a Q sigma, lt P a Q -> P = P0[sigma] ->
 
 
 
+(*==========   this is the "by individual actions" attempt    ===============*)
 
-
-
+(* it seems like for tau-actions, we need to have the inversion for
+   both senders and receptors in order to proceed in the proof and the IH cannot be used   as it's protected by the condition that the action must be a tau-action.
+  so i decided to prove first the inversion for send and receive actions.
+  It appears that for the send version, no conditions on sigma is necessary. 
+  While for the receive version, we need to require surjectivity on sigma but
+  that requirement also makes us stuck since lifting doesn't seem to preserve
+  surjectivity.
+*)
 
 
 Lemma invert_sblt_send: forall P P0 a Q sigma x y, a = Lsend x y -> lt P a Q  -> 
@@ -403,84 +350,6 @@ induction Hlt; intros; inversion Hlsend.
   auto. destruct H0. inversion H0.
 Qed.
 
-
-
-
-
-
-Lemma BOOGERS: forall P P0 a Q sigma x y, a = Lsend x y -> lt P a Q  -> 
-  P = P0[sigma] -> injective sigma  -> 
-  exists P' a0, Q = P'[sigma] /\  a = a0[sigma] /\  lt P0 a0 P'. 
-Proof.
-intros P P0 a Q sigma x y Hlsend Hlt Hsb Hinj.
-generalize dependent x.
-generalize dependent y.
-generalize dependent sigma.
-generalize dependent P0.
-induction Hlt; intros; inversion Hlsend.   
-- set (lem:= invert_sb_send).
-  specialize (lem _ _ _ _ _ Hsb).  
-  destruct lem as [xp [yp [p lem]]].  subst. cbn in *.
-  inversion Hsb. 
-  repeat eexists; eauto with picalc; cbn; auto. 
-
-
-- set (lem:= invert_sb_par _ _ _ _ Hsb).
-  destruct lem as [p [q lem]].  subst.
-  cbn in Hsb. inversion Hsb. subst. 
-  specialize (IHHlt p sigma eq_refl Hinj _ _ eq_refl). 
-  destruct IHHlt as [p' [a0 IHHlt]].  
-  destruct IHHlt as [G1 [G2 G3]].
-  rewrite G1. 
-  exists (Par p' q). eexists. split; eauto with picalc.
-  split. apply G2. eapply Lt_parL. auto.
-  eapply (not_bdsend_sub_rev _ sigma).
-  symmetry in G2. rewrite G2. auto.
-
-- set (lem:= invert_sb_par _ _ _ _ Hsb).  
-  destruct lem as [p [q lem]]. subst. cbn in Hsb.
-  inversion Hsb. subst.
-  specialize (IHHlt q sigma eq_refl Hinj _ _ eq_refl). 
-  destruct IHHlt as [q' [a0 IHHlt]].
-  destruct IHHlt as [G1 [G2 G3]]. 
-  rewrite G1.
-  exists (Par p q'). repeat eexists. 
-  eauto with picalc.  
-  eapply Lt_parR. auto.
-  eapply (not_bdsend_sub_rev _ sigma). 
-  symmetry in G2. rewrite G2. auto.
- 
-- subst. destruct (down_send (LbdSend x) x0 y). 
-  auto. destruct H0. inversion H0.
-
-- set (lem := invert_sb_nu  _ _ _ Hsb).
-  destruct lem as [p lem]. subst. 
-  cbn in Hsb. inversion Hsb. subst.
-  specialize (IHHlt p _ eq_refl (lift_preserve_inj sigma Hinj)). 
-  set (lem := down_send a x y Hlsend). 
-  destruct lem as [x0 [y0 lem]]. specialize (IHHlt _ _ lem).
-  destruct IHHlt as [p' [a0 IHHlt]].  
-  destruct IHHlt as [G1 [G2 G3]]. rewrite G2 in Hlsend.
-
-
-  unfold down in Hlsend. 
-  replace (fun x : nat => ch (x - 1)) with down_sb in Hlsend; auto.
-  symmetry in Hlsend.
-  rewrite Hlsend. rewrite G1.
-  rewrite G2 in H.
-  exists (Nu p'). exists (a0[down_sb]).  
-  repeat eexists; cbn; eauto with picalc.
-  erewrite down_permute_lab. auto. 
-  eapply notinlabup. apply H.
-  eapply Lt_res. apply G3. 
-  eapply notinlabup. apply H.
-  rewrite G2 in H0.
-  eapply not_bdsend_sub_rev. apply H0.
-  auto.
-
-- subst. destruct (down_send (LbdSend x) x0 y). 
-  auto. destruct H0. inversion H0.
-Qed.
 
 Lemma invert_sblt_rcv: forall P P0 a Q sigma x y, a = Lrcv x y -> lt P a Q  -> 
   P = P0[sigma] -> surjective sigma  -> 
@@ -559,13 +428,7 @@ induction Hlt; intros; inversion Hlrcv.
 Admitted.
 
 
-
-
-
-
-
-
-(* /!\ for tau   
+(* /!\ for tau*)   
 Lemma invert_sblt_tau: forall P P0 a Q sigma, a = Ltau -> lt P a Q  -> 
   P = P0[sigma] -> injective sigma  -> 
   exists P' a0, Q = P'[sigma] /\  a = a0[sigma] /\  lt P0 a0 P'. 
@@ -574,7 +437,6 @@ intros P P0 a Q sigma Hltau Hlt Hsb Hinj.
 generalize dependent sigma.
 generalize dependent P0.
 induction Hlt; intros; inversion Hltau.   
- 
 
 - set (lem:= invert_sb_par _ _ _ _ Hsb).
   destruct lem as [p [q lem]].  subst.
@@ -604,63 +466,16 @@ induction Hlt; intros; inversion Hltau.
  
 - set (lem:= invert_sb_par _ _ _ _ Hsb).  
   destruct lem as [p [q lem]]. subst. cbn in Hsb.
-  inversion Hsb. subst.
+  inversion Hsb. subst. admit.
+  (*
   specialize (IHHlt1 _ _ eq_refl Hsb). destruct IHlt1.
   specialize (IHHlt2 _ _ eq_refl Hsb). destruct IHlt2.
-  clear H7 H5 H3.
-  assert (not_bdsend (Lsend x y)); eauto with picalc.
-  assert (not_bdsend (Lrcv x y)); eauto with picalc.
-  specialize (H6 H5). specialize (H4 H3). clear H3 H5.
-  destruct H4 as [p' [a0 H4]]. destruct H6 as [q' [b0 H6]].
-  destruct H4 as [F1 [F2 F3]]. destruct H6 as [G1 [G2 G3]].   
-  subst. 
-  exists (Par p' q'). exists Ltau. cbn. split; eauto with picalc. 
-  split. auto. 
-  destruct (sb_send _ _ _ _ F2). destruct (sb_rcv _ _ _ _ G2). 
-  destruct H3. destruct H4. subst. cbn in *.
-  inversion F2. inversion G2. subst.
-  (*note that we used injectivity here*)
-  unfold super_bij in H2. destruct H2.
-  assert (x0 = x1).  apply H3. auto.
-  assert (x2 = x3).  apply H3. auto. 
-  subst. eauto with picalc.
-
-- set (lem:= invert_sb_par _ _ _ _ H1).  
-  destruct lem as [p [q lem]]. subst. cbn in H1.
-  inversion H1. subst.
-  specialize (IHlt1 _ _ eq_refl H2). destruct IHlt1.
-  specialize (IHlt2 _ _ eq_refl H2). destruct IHlt2.
-  clear H7 H5 H3.
-  assert (not_bdsend (Lsend x y)); eauto with picalc.
-  assert (not_bdsend (Lrcv x y)); eauto with picalc.
-  specialize (H4 H5). specialize (H6 H3). clear H3 H5.
-  destruct H4 as [p' [a0 H4]]. destruct H6 as [q' [b0 H6]].
-  destruct H4 as [F1 [F2 F3]]. destruct H6 as [G1 [G2 G3]].   
-  subst. 
-  exists (Par p' q'). exists Ltau. cbn. split; eauto with picalc. 
-  split. auto. 
-  destruct (sb_send _ _ _ _ G2). destruct (sb_rcv _ _ _ _ F2). 
-  destruct H3. destruct H4. subst. cbn in *.
-  inversion F2. inversion G2. subst.
-  (*note that we used injectivity here*)
-  unfold super_bij in H2. destruct H2.
-  assert (x0 = x1).  apply H3. auto.
-  assert (x2 = x3).  apply H3. auto. 
-  subst. eauto with picalc.
-
-*)
+  *)
+Admitted.
 
 
 
-
-
-
-
-
-
-
-
-
+(*========== attempt requiring sigma to be a bijection   ===============*)
 
 Lemma invert_sb_lt: forall P P0 a Q sigma, lt P a Q -> 
   P = P0[sigma] -> super_bij sigma ->  
@@ -815,11 +630,11 @@ induction H; split; intros.
   destruct IHlt as [G1 [G2 G3]]. subst.
  (*-----------------------------------*)
   *)
+  Admitted.
 
 
 
-(*
-Lemma invert_sb_lt: forall P P0 a Q sigma, lt P a Q -> 
+Lemma invert_sb_lt2: forall P P0 a Q sigma, lt P a Q -> 
   P = P0[sigma] -> super_bij sigma ->  
   (not_bdsend a -> exists P' a0, Q = P'[sigma] /\  a = a0[sigma] /\  lt P0 a0 P') /\
   (forall x0, a = LbdSend x0 ->  exists P' a0, Q = P'[up sigma] /\  a = a0[sigma] /\  lt P0 a0 P') . 
@@ -962,7 +777,7 @@ induction H; split; intros.
   inversion H2. subst.  
   specialize (IHlt _ _ eq_refl H3). destruct IHlt.
 
-*)
+(**)
 
 
 
@@ -988,3 +803,81 @@ induction H; split; intros.
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+(* -------------------------------------------------------------
+   This attempt obviously doesn't work since the lt-renaming lemma
+   for free and bounded actions work differently.
+   ------------------------------------------------------------
+Lemma invert_sb_lt: forall P P0 a Q sigma, lt P a Q -> 
+  P = P0[sigma] -> super_bij sigma ->  
+  exists P' a0, Q = P'[sigma] /\  a = a0[sigma] /\  lt P0 a0 P'. 
+Proof.
+intros. 
+generalize dependent sigma.
+generalize dependent P0.
+induction H; intros.   
+- set (lem:= invert_sb_send).
+  specialize (lem _ _ _ _ _ H0).
+  destruct lem. do 2 destruct H. subst. cbn in *.
+  inversion H0. 
+  eexists. exists (Lsend x0 x1). 
+  split; eauto with picalc. 
+
+-set (lem:= invert_sb_rcv).
+  specialize (lem _ _ _ _ H0).
+  destruct lem. destruct H as [p H]. subst. cbn in *.
+  inversion H0.
+  unfold super_bij in H1. destruct H1.
+  assert (exists y0, y = y0[sigma]). eapply H.
+  destruct H4 as [y0 H4].
+  exists (p[y0 ..]). exists (Lrcv x0 y0). 
+  split; cbn; try erewrite up_beta_simpl_pr; rewrite H4; eauto with picalc.
+
+- set (lem:= invert_sb_par).
+  specialize (lem _ _ _ _ H1).
+  destruct lem as [P1 [Q1 lem]].  subst.
+  cbn in H1. inversion H1. rewrite H4 in IHlt.
+  specialize (IHlt P1 sigma eq_refl H2).
+  destruct IHlt as [P'0 [a0 IHlt]].  destruct IHlt.
+  destruct H6.  rewrite H3. 
+  exists (Par P'0 Q1). eexists. split; eauto with picalc.
+  split. apply H6. eapply Lt_parL. auto.
+  eapply (not_bdsend_sub_rev _ sigma).
+  symmetry in H6. rewrite H6. auto.
+
+- set (lem:= invert_sb_par _ _ _ _ H1).  
+  destruct lem as [p [q lem]]. subst. cbn in H1.
+  inversion H1. subst.
+  specialize (IHlt _ _ eq_refl H2).
+  destruct IHlt as [P' [a0 IHlt]].
+  destruct IHlt as [IHlt1 [IHlt2 IHlt3]].
+  rewrite IHlt1.
+  exists (Par p P'). repeat eexists. 
+  eauto with picalc. 
+  eapply Lt_parR. auto.
+  eapply (not_bdsend_sub_rev _ sigma).
+  symmetry in IHlt2. rewrite IHlt2. auto.
+
+ 
+- set (lem:= invert_sb_par _ _ _ _ H1).  
+  destruct lem as [p [q lem]]. subst. cbn in H1.
+  inversion H1. subst.
+  specialize (IHlt _ _ eq_refl H2).
+  destruct IHlt as [p' [a0 IHlt]].
+  destruct IHlt as [IHlt1 [IHlt2 IHlt3]].
+  rewrite IHlt1. 
+ *)
